3b7b037ddd0cdb597951e3a405e474452c09a044
Jadw1 <jadw1_1@o2.pl>
2020-12-15 16:16:20 +0100

    less logging

--- prinz/src/main/scala/pl/touk/nussknacker/prinz/util/config/ConfigReader.scala
+++ prinz/src/main/scala/pl/touk/nussknacker/prinz/util/config/ConfigReader.scala
@@ -11,14 +11,14 @@ object ConfigReader {
 
   def getConfigValue[T](path: String, default: T, extractor: (Config, String) => T)(implicit config: Config, basePath: String): T = {
     val fullPath = s"$basePath$path"
-    logger.info(s"Config used is ${config.toString}")
+    //logger.info(s"Config used is ${config.toString}")
     if (config.hasPath(fullPath)) {
       val extracted = extractor(config, fullPath)
-      logger.info("Config value {} defined with value {}", fullPath, extracted)
+      //logger.info("Config value {} defined with value {}", fullPath, extracted)
       extracted
     }
     else {
-      logger.info("No config value {} defined. Using default value {}", fullPath, default)
+      //logger.info("No config value {} defined. Using default value {}", fullPath, default)
       default
     }
   }
